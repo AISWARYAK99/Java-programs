fe9b0c34333cb58c3bd59156436f36f374295cb2
AISWARYA K <66054986+AISWARYAK99@users.noreply.github.com>
2021-03-06 11:47:55 +0530

    Conversion
    programs on converting integer to binary.

---
+++ Shift1.java
@@ -0,0 +1,14 @@
+class Shift1
+{
+	public static void main(String[] args)
+	{
+		int a=7;
+		int b=70;
+		int c=2;
+		int d=-2;
+		System.out.println("Input Binary of "+a+"  "+Integer.toBinaryString(a));
+		System.out.println("Input Binary of "+b+"  "+Integer.toBinaryString(b));
+		System.out.println("Input Binary of "+c+"  "+Integer.toBinaryString(c));
+		System.out.println("Input Binary of "+d+"  "+Integer.toBinaryString(d));
+	}
+}
